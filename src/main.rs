1010eec450bff4352add45c58df1faa53f29e328
Marshall Ku <marshall@kakao.com>
2024-07-30 22:34:07 +0900

    Add trace logs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use axum::serve;
 use routes::app::app;
 use tokio::net::TcpListener;
-use tracing::info;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::{info, Level};
+use utils::log::trace_layer_on_request;
 
 mod routes;
+mod utils;
 
 #[tokio::main]
 async fn main() {
-    let app = app();
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    let app = app().layer(
+        TraceLayer::new_for_http()
+            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(DefaultOnResponse::new().level(Level::INFO))
+            .on_request(trace_layer_on_request),
+    );
     let address = "127.0.0.1:18080";
     let listener = TcpListener::bind(address).await.unwrap();
 
